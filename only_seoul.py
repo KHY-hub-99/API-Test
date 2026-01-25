79118d04fcd2c43983a2f13b8ce6a7103e67c99e
김현용 <multikhy0612@gmail.com>
2026-01-26 03:25:06 +0900

    commit

--- only_seoul.py
+++ only_seoul.py
@@ -862,7 +862,7 @@ def optimize_day(places, restaurants, fixed_events, start_time_str, target_date_
                 "name": node["name"],
                 "category": node["category"],
                 "time": time_str,
-                "transit_info": transit_info
+                "transit_to_here": transit_info
             })
 
     return timeline
